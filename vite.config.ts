d4646fb3b0c7ebfa94ea9338a6d8f47803c24bbe
Anna Skuratova <anna@molnix.com>
2023-01-08 14:09:11 +0200

    fix/typo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ranger-slider",
+  base: "/range-slider",
   plugins: [vue()],
   resolve: {
     alias: {
